b92332931e03849b33a912a2869539ce9ae60d9c
sergiJimenez <acasualcolumbus@gmail.com>
2023-03-14 22:52:30 +0100

    HOTFIX

--- src/calculator.js
+++ src/calculator.js
@@ -5,17 +5,21 @@ var Calculator = /** @class */ (function () {
     function Calculator() {
     }
     Calculator.prototype.addNumbers = function (firstNumber, secondNumber) {
-        console.log("".concat(firstNumber + secondNumber)); // tslint:disable-next-line:no-console
+        // tslint:disable-next-line:no-console
+        console.log("".concat(firstNumber + secondNumber));
     };
     Calculator.prototype.substractNumbers = function (firstNumber, secondNumber) {
-        console.log("".concat(firstNumber - secondNumber)); // tslint:disable-next-line:no-console
+        // tslint:disable-next-line:no-console
+        console.log("".concat(firstNumber - secondNumber));
     };
     Calculator.prototype.multiplyNumbers = function (firstNumber, secondNumber) {
-        console.log("".concat(firstNumber * secondNumber)); // tslint:disable-next-line:no-console
+        // tslint:disable-next-line:no-console
+        console.log("".concat(firstNumber * secondNumber));
     };
     Calculator.prototype.divideNumbers = function (firstNumber, secondNumber) {
         if (secondNumber !== 0) {
-            console.log("".concat(firstNumber / secondNumber)); // tslint:disable-next-line:no-console
+            // tslint:disable-next-line:no-console
+            console.log("".concat(firstNumber / secondNumber));
         }
         else {
             console.log('You can not divide with 0... Try to type another one.');
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,16 +1,20 @@
 export class Calculator {
     public addNumbers(firstNumber: number, secondNumber: number): void {
-        console.log(`${firstNumber + secondNumber}`); // tslint:disable-next-line:no-console
+        // tslint:disable-next-line:no-console
+        console.log(`${firstNumber + secondNumber}`);
     }
     public substractNumbers(firstNumber: number, secondNumber: number): void {
-        console.log(`${firstNumber - secondNumber}`); // tslint:disable-next-line:no-console
+        // tslint:disable-next-line:no-console
+        console.log(`${firstNumber - secondNumber}`);
     }
     public multiplyNumbers(firstNumber: number, secondNumber: number): void {
-        console.log(`${firstNumber * secondNumber}`); // tslint:disable-next-line:no-console
+        // tslint:disable-next-line:no-console
+        console.log(`${firstNumber * secondNumber}`);
     }
     public divideNumbers(firstNumber: number, secondNumber: number): void {
         if (secondNumber !== 0) {
-            console.log(`${firstNumber / secondNumber}`); // tslint:disable-next-line:no-console
+            // tslint:disable-next-line:no-console
+            console.log(`${firstNumber / secondNumber}`); 
         } else {
             console.log('You can not divide with 0... Try to type another one.');
         }
