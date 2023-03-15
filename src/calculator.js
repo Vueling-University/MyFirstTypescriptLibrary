44803f1276ecb6783e2025585ddfe158b0ce568e
sergiJimenez <acasualcolumbus@gmail.com>
2023-03-15 10:26:02 +0100

    HOTFIX

--- src/calculator.js
+++ src/calculator.js
@@ -5,25 +5,31 @@ var Calculator = /** @class */ (function () {
     function Calculator() {
     }
     Calculator.prototype.addNumbers = function (firstNumber, secondNumber) {
+        firstNumber + secondNumber;
         // tslint:disable-next-line:no-console
-        console.log("".concat(firstNumber + secondNumber));
+        // console.log(`${firstNumber + secondNumber}`);
     };
     Calculator.prototype.substractNumbers = function (firstNumber, secondNumber) {
+        firstNumber - secondNumber;
         // tslint:disable-next-line:no-console
-        console.log("".concat(firstNumber - secondNumber));
+        // console.log(`${firstNumber - secondNumber}`);
     };
     Calculator.prototype.multiplyNumbers = function (firstNumber, secondNumber) {
+        firstNumber * secondNumber;
         // tslint:disable-next-line:no-console
-        console.log("".concat(firstNumber * secondNumber));
+        // console.log(`${firstNumber * secondNumber}`);
     };
     Calculator.prototype.divideNumbers = function (firstNumber, secondNumber) {
         if (secondNumber !== 0) {
+            firstNumber / secondNumber;
             // tslint:disable-next-line:no-console
-            console.log("".concat(firstNumber / secondNumber));
+            // console.log(`${firstNumber / secondNumber}`);
         }
         else {
             // tslint:disable-next-line:no-console
-            console.log('You can not divide with 0... Try to type another one.');
+            // console.log('');
+            var errorMessage = "You can not divide with 0... Try to type another one.";
+            errorMessage;
         }
     };
     return Calculator;
