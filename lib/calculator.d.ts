d114dc286b636aedaf40c464ddcf96bb14f84a7a
sergiJimenez <acasualcolumbus@gmail.com>
2023-03-15 10:41:59 +0100

    HOTFIX

--- lib/calculator.d.ts
+++ lib/calculator.d.ts
@@ -1,4 +1,5 @@
 export declare class Calculator {
+    errorMessage: string;
     addNumbers(firstNumber: number, secondNumber: number): void;
     substractNumbers(firstNumber: number, secondNumber: number): void;
     multiplyNumbers(firstNumber: number, secondNumber: number): void;
